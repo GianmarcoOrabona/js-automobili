3dc4af67e1c69fd998c12032fd9b1009279f6d4d
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-22 13:59:51 +0100

    add 3 different arrays

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+/* 
+Dividi le automobili in 3 array separati:
+nel primo array solo le auto a benzina,
+nel secondo solo le auto a diesel,
+nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array.
+*/
+
 // Creo l'array di 10 automobili
 const cars = [
     { marca: 'Toyota', modello: 'Yaris', alimentazione: 'ibrida' },
@@ -9,5 +17,35 @@ const cars = [
     { marca: 'Hyundai', modello: 'Kona', alimentazione: 'elettrica' },
     { marca: 'Fiat', modello: '500', alimentazione: 'GPL' },
     { marca: 'Mercedes-Benz', modello: 'C-Class', alimentazione: 'diesel' },
-    { marca: 'Volvo', modello: 'XC90', alimentazione: 'ibrida' },
+    { marca: 'Chevrolet', modello: 'Camaro', alimentazione: 'benzina' },
 ];
+
+// Creo degli array vuoti dove pushare le auto
+let gasolineCars = [];
+let dieselCars = [];
+let otherCars = [];
+
+// Con un ciclo forEach() scansiono tutte le auto e le loro relative alimentazioni
+cars.forEach(function (car) {
+    if (car.alimentazione == 'benzina') {
+        gasolineCars.push(car);
+    } else if (car.alimentazione == 'diesel') {
+        dieselCars.push(car);
+    } else {
+        otherCars.push(car);
+    }
+});
+
+// Stampo in console gli array creati
+
+/* Macchine a benzina */
+console.log('Macchine a benzina');
+console.log(gasolineCars);
+
+/* Macchine a diesel */
+console.log('Macchine a diesel');
+console.log(dieselCars);
+
+/* Macchine con altra alimentazione */
+console.log('Macchine con altra alimentazione');
+console.log(otherCars);
