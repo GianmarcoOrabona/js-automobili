ae7a7e56fd5d215b9b018519c2e5865c927c684f
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-22 13:14:29 +0100

    add array

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Creo l'array di 10 automobili
+const cars = [
+    { marca: 'Toyota', modello: 'Yaris', alimentazione: 'ibrida' },
+    { marca: 'Tesla', modello: 'Model S', alimentazione: 'elettrica' },
+    { marca: 'BMW', modello: 'X5', alimentazione: 'ibrida' },
+    { marca: 'Ford', modello: 'Focus', alimentazione: 'benzina' },
+    { marca: 'Audi', modello: 'A3', alimentazione: 'diesel' },
+    { marca: 'Nissan', modello: 'Leaf', alimentazione: 'elettrica' },
+    { marca: 'Hyundai', modello: 'Kona', alimentazione: 'elettrica' },
+    { marca: 'Fiat', modello: '500', alimentazione: 'GPL' },
+    { marca: 'Mercedes-Benz', modello: 'C-Class', alimentazione: 'diesel' },
+    { marca: 'Volvo', modello: 'XC90', alimentazione: 'ibrida' },
+];
